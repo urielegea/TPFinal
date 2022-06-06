083eb40ef57925b64fb170ff9719036e79badf90
Urii <egeauriel@gmail.com>
2022-06-06 19:26:39 -0300

    0.10

--- src/com/company/Class/NuevoProfesional.java
+++ src/com/company/Class/NuevoProfesional.java
@@ -4,9 +4,30 @@ import java.util.HashMap;
 
 public interface NuevoProfesional {
 
-    public default Profesional crearProfesional(String nombre, String apellido, String dni, String telefono, String cuenta,
-                                                String clave, String fechaAlta, HashMap<String, Paciente> pacienteLista){
-        return new Profesional(nombre,apellido,dni,telefono,cuenta,clave,fechaAlta,pacienteLista);
+    public default Profesional crearProfesional(
+            String nombre,
+            String apellido,
+            String dni,
+            String telefono,
+            String cuenta,
+            String clave,
+            String fechaAlta,
+            HashMap<String, Paciente> pacienteLista,
+            HashMap<String, Usuario> usuariosHashMap){
+
+        if(!buscarProfesionalConstructor(usuariosHashMap, cuenta)){
+            return new Profesional(nombre,apellido,dni,telefono,cuenta,clave,fechaAlta,pacienteLista);
+        }else{
+            return null;
+        }
+    }
+
+    public default boolean buscarProfesionalConstructor(HashMap<String, Usuario> usuariosHashMap, String cuenta){
+        boolean flag = false;
+        if(!usuariosHashMap.containsKey(cuenta)){
+            flag = true;
+        }
+    return flag;
     }
 
 }
