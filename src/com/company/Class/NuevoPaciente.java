e33cffb1760d5d324a3a039bbe68930e3a7d7e1f
Urii <egeauriel@gmail.com>
2022-06-06 17:05:52 -0300

    0.9

--- src/com/company/Class/NuevoPaciente.java
+++ src/com/company/Class/NuevoPaciente.java
@@ -1,4 +1,9 @@
 package com.company.Class;
 
 public interface NuevoPaciente {
+
+    public default Paciente crearPaciente(String nombre, String apellido, String dni, String telefono, String cuenta,
+                                          String clave, String fechaAlta, String numeroHistorial){
+        return new Paciente(nombre,apellido,dni,telefono,cuenta,clave,fechaAlta,numeroHistorial);
+    }
 }
