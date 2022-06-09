20584a22b695a5222934c36f4bfbd94a6e983512
Urii <egeauriel@gmail.com>
2022-06-09 18:40:33 -0300

    0.12

--- src/com/company/Class/Interfaces/AdministrarEnfermedades.java
+++ src/com/company/Class/Interfaces/AdministrarEnfermedades.java
@@ -3,11 +3,23 @@ package com.company.Class.Interfaces;
 import com.company.Class.Enfermedad;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface AdministrarEnfermedades {
 
     public default Enfermedad crearEnfermedad(String nombre, ArrayList<String> tareaDeControlLista,
-                                              String descripcion, int duracionDias){
-        return new Enfermedad(nombre,tareaDeControlLista,descripcion,duracionDias);
+                                              String descripcion, int duracionDias, ArrayList<Enfermedad> enfermedadLista){
+        boolean flag = false;
+        for(Enfermedad e : enfermedadLista) {
+            if (e.getNombre().compareToIgnoreCase(nombre) == 0) {
+                flag = true;
+            }
+        }
+        if(!flag) {
+            return new Enfermedad(nombre, tareaDeControlLista, descripcion, duracionDias);
+        }
+        return null;
     }
+
+
 }
