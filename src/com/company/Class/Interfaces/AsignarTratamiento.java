5694b26a870e11cc168b41f5efa74dd9b78a1160
MatiasValenzisi <91572020+MatiasValenzisi@users.noreply.github.com>
2022-06-12 17:25:40 -0300

    generar token tratamiento

--- src/com/company/Class/Interfaces/AsignarTratamiento.java
+++ src/com/company/Class/Interfaces/AsignarTratamiento.java
@@ -1,8 +1,8 @@
 package com.company.Class.Interfaces;
-
 import java.util.HashMap;
 import java.util.Random;
 import com.company.Class.HistorialMedico;
+import com.company.Class.Tratamiento;
 
 public interface AsignarTratamiento {
 	
@@ -27,7 +27,20 @@ public interface AsignarTratamiento {
 	// Genera un token unico para el tratamiento. El mismo es de 10 caracteres.
 	
 	public default String generateTokenTratamiento(HashMap<String, HistorialMedico> historialMedicoHashMap) {
-		return null;
+		String tokenTratamiento = "";
+		boolean flag = true;
+		while (flag) {
+			flag = false;
+			tokenTratamiento = generateToken();
+			for (HistorialMedico historialMedico : historialMedicoHashMap.values()) {
+				for (Tratamiento tratamiento : historialMedico.getTratamientoLista()) {
+					if (tratamiento.getToken().compareTo(tokenTratamiento) == 0) {
+						flag = true;
+					}
+				}
+			}
+		}		
+		return tokenTratamiento;
 	}
 
 	// Genera un numero con los digitos del 0 al 9 con una cantidad de 10 caracteres con retorno String.
