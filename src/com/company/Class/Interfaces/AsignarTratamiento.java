e3cce2da2fc502b49b21bb827dcc70223ca76746
MatiasValenzisi <91572020+MatiasValenzisi@users.noreply.github.com>
2022-06-12 16:59:03 -0300

    Generar token al azar

--- src/com/company/Class/Interfaces/AsignarTratamiento.java
+++ src/com/company/Class/Interfaces/AsignarTratamiento.java
@@ -1,7 +1,7 @@
 package com.company.Class.Interfaces;
 
 import java.util.HashMap;
-
+import java.util.Random;
 import com.company.Class.HistorialMedico;
 
 public interface AsignarTratamiento {
@@ -29,5 +29,19 @@ public interface AsignarTratamiento {
 	public default String generateTokenTratamiento(HashMap<String, HistorialMedico> historialMedicoHashMap) {
 		return null;
 	}
+
+	// Genera un numero con los digitos del 0 al 9 con una cantidad de 10 caracteres con retorno String.
+	
+	public default String generateToken() {				
+		String token = "";
+		int min = 0, max = 10, cant = 0;
+		Random random = new Random();
+		while (cant < 10) {
+			int value = random.nextInt(max + min) + min;
+			token = token+""+value;
+			cant++;
+		}
+		return token;
+	}	
 	
 }
